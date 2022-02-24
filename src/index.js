cac9e330958244f2490c676ab8a13e87fabdb63e
fortune7s <yuliya19891986@rambler.ru>
2022-02-25 00:49:29 +0300

    feat: add task braskets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 != 0) return false;
+
+
+  let brackets = [];
+   
+  for (let i = 0; i < str.length; i++) {
+     for (let j = 0; j < bracketsConfig.length; j++) {
+        let elemStart = bracketsConfig[j][0];
+        let elemEnd = bracketsConfig[j][1];
+
+        if (str[i] === elemStart) {
+           if (str[i] === elemEnd && brackets[brackets.length - 1] === elemEnd) {
+              brackets.pop();
+           } else {
+              brackets.push(elemStart);
+           }
+          
+        } else if (str[i] === elemEnd ) {
+           if (brackets.length === 0) return false;
+           if (brackets[brackets.length - 1] === elemStart) {
+              brackets.pop();
+              
+           }   
+        }
+     }
+  }
+
+  if (brackets.length > 0) return false;
+
+  return true;
+
+
+  
 }
