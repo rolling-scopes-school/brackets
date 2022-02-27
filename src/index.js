f3c38ae8c56788b11c92524f6f0079b434e425ef
soola-sys <agdarbekovsultan@gmail.com>
2022-02-27 19:56:18 +0600

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  bracketsConfig.map((el) => {
+    stack.push(el.join(""));
+  });
+
+  let i = 0;
+  do {
+    let current = stack[i];
+    if (str.includes(current)) {
+      str = str.replace(current, "");
+      i = -1;
+    }
+    i++;
+  } while (i < stack.length);
+
+  return str.length === 0;
+};
