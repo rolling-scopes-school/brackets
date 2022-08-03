f60228c1f6c66a48a842f06a5c9dfcf8654e06da
tkornilova <kornilova.tat07@gmail.com>
2022-08-04 01:02:02 +0400

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  for (let i = 0; i <= str.length; i++) {
+    bracketsConfig.map(bracket => {
+      if (str[i] === bracket[0] && str[i + 1] === bracket[1]) {
+        str.splice(i, 2);
+        i = i - 2;
+      }
+    });
+  }
+
+  if (str.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
