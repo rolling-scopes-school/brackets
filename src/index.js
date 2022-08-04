9faee723408e3a4c3713284d2555053f00f27c7c
Alex M <91932617+AlexMingal@users.noreply.github.com>
2022-08-04 20:01:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  
+    let stack = [];
+
+      for (let i = 0; i < str.length; i++) {
+      let curSum = str[i];
+      
+      if (bracketsConfig.find(el => el[1] === curSum) && stack.length !== 0) {
+        
+        if (stack.length === 0) {
+          
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+        
+        bracketsConfig.forEach((el) => {
+          if (el[1] === curSum) {
+           
+            if (topElement === el[0]) {
+             
+              stack.pop();
+              
+            } else {
+                if (el[1] === el[0]) {
+                    stack.push(el[1])
+                }
+              return false;
+            }
+          }
+        })
+      } else {
+        stack.push(curSum);     
+      }
+    }
+    return stack.length === 0
+  }
