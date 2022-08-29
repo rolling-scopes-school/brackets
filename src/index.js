badeff185c6c194445e61d618153eafb6438ef73
marinakhotko <kudzelkamarina@gmail.com>
2022-08-29 17:52:22 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = [str, bracketsConfig].toString();
+
+  let allBrackets = ['[', '{', '(', ']', '}', ')','|']
+  let open_brackets = ['[', '{', '(']
+  let library ={
+    ['}'] : '{',
+    [')'] : '(',
+    [']'] : '[',
+    ['|'] : '|',
+  }
+  let stack = [];
+  let strLast ="";
+
+  for (let i = 0; i< a.length; i++){
+    if (allBrackets.includes(a[i]) === true) {
+      strLast = strLast + a[i];
+    }
+  }
+
+ //
+
+  for (let i = 0; i< strLast.length; i++){
+
+    let currantSimvol = strLast[i];
+
+    if (open_brackets.includes(currantSimvol)){
+      stack.push(currantSimvol);
+    }
+    else {
+     
+                  if (stack.length === 0){ 
+                      if (currantSimvol === '|')
+                        stack.push(currantSimvol);
+                      else{
+                        return false;
+                      }
+                  }                
+
+           let topElement = stack[stack.length - 1];
+           
+                  if (library[currantSimvol] === topElement){
+                      stack.pop();
+                  }
+                  if (library[currantSimvol] !== topElement && currantSimvol === '|'){
+                    stack.push(currantSimvol);
+                  }
+                  
+          }
+  }
+  
+  return (stack.length === 0 ? true : false);
 }
