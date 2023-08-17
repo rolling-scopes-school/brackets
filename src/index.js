8ed4e104d3c677a84789d9b7a9cae2bc52145658
sinfaint00 <122840916+sinfaint00@users.noreply.github.com>
2023-08-17 18:21:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const symbolPairs = Object.fromEntries(bracketsConfig);
 
+  for (let symbol of str) {
+    if (symbolPairs[symbol]) {
+      if (symbolPairs[symbol] === symbol) {
+        // Handle cases where opening and closing symbols are the same
+        if (stack.length === 0 || stack[stack.length - 1] !== symbol) {
+          stack.push(symbol);
+        } else {
+          stack.pop();
+        }
+      } else {
+        // Handle opening symbols
+        stack.push(symbol);
+      }
+    } else {
+      // Handle closing symbols
+      if (stack.length === 0 || symbolPairs[stack.pop()] !== symbol) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(check('()', [['(', ')']])); // true
+console.log(check('[[[]]][]', [['[', ']']])); // true
+console.log(check('][', [['[', ']']])); // false
+console.log(check('{}{}', [['{', '}']])); // true
+console.log(check('{{{}}}', [['{', '}']])); // true
+console.log(check('}{', [['{', '}']])); // false
+console.log(check('{{}}{}', [['{', '}'], ['{', '}']])); // true
+console.log(check('||', [['|', '|']])); // true
+console.log(check('|()|', [['(', ')'], ['|', '|']])); // true
+console.log(check('|(|)', [['(', ')'], ['|', '|']])); // false
+console.log(check('|()|(||)||', [['(', ')'], ['|', '|']])); // true
