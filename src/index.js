0334902f4807471772fd4c9c4846fc4928f573d3
Ruben Martirosyan <98277521+RUBBOSS@users.noreply.github.com>
2023-10-26 10:26:28 +0400

    init:finished work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = new Set();
+  const closingBrackets = new Set();
+  const matchingBrackets = {};
+
+  for (const [open, close] of bracketsConfig) {
+    openingBrackets.add(open);
+    closingBrackets.add(close);
+    matchingBrackets[close] = open;
+  }
+
+  for (const char of str) {
+    if (openingBrackets.has(char)) {
+      if (char !== matchingBrackets[char] || !stack.includes(char)) {
+        stack.push(char);
+      } else {
+        const lastBracket = stack.pop();
+        if (lastBracket !== char) {
+          return false;
+        }
+      }
+    } else if (closingBrackets.has(char)) {
+      if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
