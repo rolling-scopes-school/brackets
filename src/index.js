f4959efdbdd2be5410fc7535fd3560cf2e72bb45
Alexander <150794381+Maximus9991982@users.noreply.github.com>
2024-09-04 14:10:35 +0300

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,6 @@ module.exports = function check(str, bracketsConfig) {
   
   string = string.replaceAll(",", "");
   
-//   string = Array.of(string);
-  
-//   string.sort();  
-  
-//   str = Array.of(str);
-  console.log(str);  
-  console.log(string);  
   
   for (i = 0; i < string.length - 1; i+=2) {
       
@@ -26,14 +19,9 @@ module.exports = function check(str, bracketsConfig) {
       for (let j = 0; j < str.length; j++) {
           if (str[j] == string[i+1]) bracket1--;
       }
-      console.log(bracket1);  
-  }
-  
-  console.log(str[(str.length / 2 - 1)]);
-  console.log(str[(str.length / 2)]);
 
-  console.log((str[(str.length / 2 - 1)] == str[(str.length / 2)]));
-  
+  }
+    
   if (bracket1 == 0) return true;
   else return false;
   
