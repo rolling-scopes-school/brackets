25db873e2e9bcb056b8585366257b5f25b8dafbb
Alexander <150794381+Maximus9991982@users.noreply.github.com>
2024-09-04 13:48:54 +0300

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = null;
+  let string = '';
+  let bracket1 = 0;
+  
+  for (let i = 0; i <= bracketsConfig.length - 1; i++) {
+      string += bracketsConfig[i].join();
+  }
+  
+  string = string.replaceAll(",", "");
+  
+//   string = Array.of(string);
+  
+//   string.sort();  
+  
+//   str = Array.of(str);
+  console.log(str);  
+  console.log(string);  
+  
+  for (i = 0; i < string.length - 1; i+=2) {
+      
+      for (let j = 0; j < str.length; j++) {
+          if (str[j] == string[i]) bracket1++;
+      }
+      console.log(bracket1);
+      for (let j = 0; j < str.length; j++) {
+          if (str[j] == string[i+1]) bracket1--;
+      }
+      console.log(bracket1);  
+  }
+  
+  if (bracket1 == 0) return true;
+  else return false;
+  
 }
